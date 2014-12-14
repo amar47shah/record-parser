8952b94b8d74af616fc8ce9c01682be5f6a28cdd
Amar Shah <lssdamperinspector@gmail.com>
2014-12-14 17:28:24 -0500

    Add pending step definitions for input feature

---
+++ features/step_definitions/input_steps.rb
@@ -0,0 +1,15 @@
+Given /^I have not started a session$/  do
+  pending
+end
+
+When /^I start a session with a file called 'data\/records\.txt'$/  do
+  pending
+end
+
+Then /^I should see 'Welcome to the Record Parser'$/  do
+  pending
+end
+
+Then /^I should see 'Parsing data\/records\.txt'$/  do
+  pending
+end
