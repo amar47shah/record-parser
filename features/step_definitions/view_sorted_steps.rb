6a9f9130c59f09e34cd59b91bb4f6ad8a377df2c
Amar Shah <lssdamperinspector@gmail.com>
2014-12-14 21:59:43 -0500

    Implement scenario steps for last name sorting

--- features/step_definitions/view_sorted_steps.rb
+++ features/step_definitions/view_sorted_steps.rb
@@ -1,11 +1,20 @@
-Given /^I have input the records "(.*?)"$/ do |arg1|
-  pending
+class FakeFile
+  attr_accessor :contents
+end
+
+def fake_file
+  @fake_file ||= FakeFile.new
+end
+
+def session
+  @session ||= RecordParser::Session.new(fake_output)
+end
+
+Given /^I have input the records "(.*?)"$/ do |records|
+  fake_file.contents = records
+  session.input(fake_file)
 end
 
 When /^My records are sorted$/ do
-  pending
-end
-
-Then /^I should see$/ do |string|
-  pending
+  session.show
 end
