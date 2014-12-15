c43221f5fdaec813b4489c0fa844d42748809f01
Amar Shah <lssdamperinspector@gmail.com>
2014-12-15 00:17:43 -0500

    Leverage World in step definitions
    Using the #sessions World function instead of accessing @session
    directly.

--- features/step_definitions/input_steps.rb
+++ features/step_definitions/input_steps.rb
@@ -2,10 +2,10 @@ Given /^I have not started a session$/ do
 end
 
 When /^I start a session$/ do
-  @session = RecordParser::Session.new(fake_output)
+  session
 end
 
 When /^I input a file called 'data\/records\.txt'$/ do
   fake_file.filename = 'data/records.txt'
-  @session.input(fake_file)
+  session.input(fake_file)
 end
