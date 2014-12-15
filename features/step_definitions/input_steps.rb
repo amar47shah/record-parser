b5091b5654ab067f2bfa370a6d5053dbd1bf58e3
Amar Shah <lssdamperinspector@gmail.com>
2014-12-14 21:20:18 -0500

    Adjust whitespace

--- features/step_definitions/input_steps.rb
+++ features/step_definitions/input_steps.rb
@@ -12,21 +12,21 @@ def fake_output
   @fake_output ||= Output.new
 end
 
-Given /^I have not started a session$/  do
+Given /^I have not started a session$/ do
 end
 
 When /^I start a session$/ do
   @session = RecordParser::Session.new(fake_output)
 end
 
-When /^I input a file called 'data\/records\.txt'$/  do
+When /^I input a file called 'data\/records\.txt'$/ do
   @session.input('data/records.txt')
 end
 
-Then /^I should see 'Welcome to the Record Parser'$/  do
+Then /^I should see 'Welcome to the Record Parser'$/ do
   expect(fake_output.messages).to include('Welcome to the Record Parser')
 end
 
-Then /^I should see 'Parsing data\/records\.txt'$/  do
+Then /^I should see 'Parsing data\/records\.txt'$/ do
   expect(fake_output.messages).to include('Parsing data/records.txt')
 end
