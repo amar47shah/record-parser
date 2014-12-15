0adb7da6abbd22ef05d8367d84d020ebb87bfae3
Amar Shah <lssdamperinspector@gmail.com>
2014-12-15 00:08:14 -0500

    Create World
    Moving Cucumber collaborator classes and global functions to CustomWorld.

--- features/step_definitions/view_sorted_steps.rb
+++ features/step_definitions/view_sorted_steps.rb
@@ -1,24 +1,3 @@
-class FakeFile
-  attr_accessor :contents
-  attr_writer :name
-
-  def read
-    @contents
-  end
-
-  def filename
-    @name
-  end
-end
-
-def fake_file
-  @fake_file ||= FakeFile.new
-end
-
-def session
-  @session ||= RecordParser::Session.new(fake_output)
-end
-
 Given /^I have input the records$/ do |records|
   fake_file.contents = records
   session.input(fake_file)
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,42 @@
 require 'record_parser'
+
+World do
+  CustomWorld.new
+end
+
+class CustomWorld
+  def fake_file
+    @fake_file ||= FakeFile.new
+  end
+
+  def fake_output
+    @fake_output ||= FakeOutput.new
+  end
+
+  def session
+    @session ||= RecordParser::Session.new(fake_output)
+  end
+end
+
+class FakeFile
+  attr_accessor :contents
+  attr_writer :name
+
+  def read
+    @contents
+  end
+
+  def filename
+    @name
+  end
+end
+
+class FakeOutput
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
