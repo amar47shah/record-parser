45b50bfe92ed4163acd26a55f9875f8e03094a9e
Amar Shah <lssdamperinspector@gmail.com>
2014-12-20 14:52:11 -0500

    Add empty Session#show_by_gender_and_last_name
    This method definition is needed to run all Cucumber features with
    logical failures instead of error failures.

--- lib/record_parser/session.rb
+++ lib/record_parser/session.rb
@@ -10,6 +10,9 @@ module RecordParser
       @contents = file.read
     end
 
+    def show_by_gender_and_last_name
+    end
+
     def show_by_last_name_descending
       @contents.lines.sort.reverse_each { |line| @out.puts line.chomp }
     end
