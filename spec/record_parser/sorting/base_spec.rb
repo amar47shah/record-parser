3072c8cff3614714b079bc527e216f67a3e4b1b5
Amar Shah <lssdamperinspector@gmail.com>
2014-12-27 23:10:48 -0500

    Remove unnecessary spec context

--- spec/record_parser/sorting/base_spec.rb
+++ spec/record_parser/sorting/base_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 module RecordParser
   module Sorting
     describe Base do
+      def expect_ordered_output(first, last)
+        expect(out).to receive(:puts).with(first).ordered
+        expect(out).to receive(:puts).with(last).ordered
+      end
+
       let(:out) { double('Out') }
       let(:file) { double('File') }
       let(:sorting) { Base.new(file, out) }
@@ -14,55 +19,48 @@ module RecordParser
         allow(file).to receive(:read).and_return(contents)
       end
 
-      describe 'showing records' do
-        def expect_ordered_output(first, last)
-          expect(out).to receive(:puts).with(first).ordered
-          expect(out).to receive(:puts).with(last).ordered
+      shared_examples 'displays heading' do |show_method, heading|
+        let(:contents) { "Record\n" }
+        it "displays a blank line before '#{heading}'" do
+          expect_ordered_output('', heading)
+          sorting.send(show_method)
         end
+        it "displays '#{heading}' before the records" do
+          expect_ordered_output(heading, 'Record')
+          sorting.send(show_method)
+        end
+      end
+      shared_examples 'shows sorted records' do |show_method, sort_method|
+        let(:contents) { "Record One\nRecord Two\n" }
+        let(:sorted) { unsorted.reverse }
+        let(:sorter) { double('Sorter') }
+        let(:unsorted) { contents.lines.map(&:chomp) }
+        before do
+          allow(Sorter).to receive(:new).with(unsorted).and_return(sorter)
+          allow(sorter).to receive(sort_method).and_return(sorted)
+        end
+        it 'displays the sorted records' do
+          expect_ordered_output(*sorted)
+          sorting.send(show_method)
+        end
+      end
+      shared_examples 'displays heading and sorted records' do |show_method, heading, sort_method|
+        it_has_behavior 'displays heading', show_method, heading
+        it_has_behavior 'shows sorted records', show_method, sort_method
+      end
 
-        shared_examples 'displays heading' do |show_method, heading|
-          let(:contents) { "Record\n" }
-          it "displays a blank line before '#{heading}'" do
-            expect_ordered_output('', heading)
-            sorting.send(show_method)
-          end
-          it "displays '#{heading}' before the records" do
-            expect_ordered_output(heading, 'Record')
-            sorting.send(show_method)
-          end
-        end
-        shared_examples 'shows sorted records' do |show_method, sort_method|
-          let(:contents) { "Record One\nRecord Two\n" }
-          let(:sorted) { unsorted.reverse }
-          let(:sorter) { double('Sorter') }
-          let(:unsorted) { contents.lines.map(&:chomp) }
-          before do
-            allow(Sorter).to receive(:new).with(unsorted).and_return(sorter)
-            allow(sorter).to receive(sort_method).and_return(sorted)
-          end
-          it 'displays the sorted records' do
-            expect_ordered_output(*sorted)
-            sorting.send(show_method)
-          end
-        end
-        shared_examples 'displays heading and sorted records' do |show_method, heading, sort_method|
-          it_has_behavior 'displays heading', show_method, heading
-          it_has_behavior 'shows sorted records', show_method, sort_method
-        end
+      describe '#show_by_last_name_descending' do
+        it_has_behavior 'displays heading and sorted records',
+                        :show_by_last_name_descending,
+                        'Sorted by last name descending:',
+                        :records_by_last_name_descending
+      end
 
-        describe '#show_by_last_name_descending' do
-          it_has_behavior 'displays heading and sorted records',
-                          :show_by_last_name_descending,
-                          'Sorted by last name descending:',
-                          :records_by_last_name_descending
-        end
-
-        describe '#show_by_gender_and_last_name' do
-          it_has_behavior 'displays heading and sorted records',
-                          :show_by_gender_and_last_name,
-                          'Sorted by gender and last name:',
-                          :records_by_gender_and_last_name
-        end
+      describe '#show_by_gender_and_last_name' do
+        it_has_behavior 'displays heading and sorted records',
+                        :show_by_gender_and_last_name,
+                        'Sorted by gender and last name:',
+                        :records_by_gender_and_last_name
       end
     end
   end
