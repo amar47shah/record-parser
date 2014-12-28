0206f72879f56a027ae055e6e52e7df244a92e87
Amar Shah <lssdamperinspector@gmail.com>
2014-12-28 00:01:43 -0500

    Correct reference to test collaborator
    Missed this in e6c795c: "fake_output" should now be called "out".

--- features/step_definitions/output_steps.rb
+++ features/step_definitions/output_steps.rb
@@ -1,4 +1,4 @@
 Then /^I should see$/ do |paragraph|
   lines = paragraph.split("\n")
-  expect(fake_output.messages.each_cons(lines.size)).to include(lines)
+  expect(out.messages.each_cons(lines.size)).to include(lines)
 end
