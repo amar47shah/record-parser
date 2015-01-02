2be1a52dcca44388ae3015939f6bee49a17705f2
Amar Shah <lssdamperinspector@gmail.com>
2015-01-01 22:32:27 -0500

    Alphabetize step definitions

--- features/step_definitions/input_steps.rb
+++ features/step_definitions/input_steps.rb
@@ -1,17 +1,17 @@
-Given /^I have chosen to sort by gender and last name$/ do
-  @instruction = 'gender-and-last-name'
-end
-
-Given /^I have chosen to sort by last name descending$/ do
-  @instruction = 'last-name-descending'
+Given /^I have chosen the bad sorting instruction "(.*?)"$/ do |bad_instruction|
+  @instruction = bad_instruction
 end
 
 Given /^I have chosen to sort by birth date$/ do
   @instruction = 'birth-date'
 end
 
-Given /^I have chosen the bad sorting instruction "(.*?)"$/ do |bad_instruction|
-  @instruction = bad_instruction
+Given /^I have chosen to sort by gender and last name$/ do
+  @instruction = 'gender-and-last-name'
+end
+
+Given /^I have chosen to sort by last name descending$/ do
+  @instruction = 'last-name-descending'
 end
 
 Given /^my file contains$/ do |contents|
