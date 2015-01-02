b0620ba834783769f1db42e8b9bdfaf152dac3cc
Amar Shah <lssdamperinspector@gmail.com>
2015-01-02 10:22:06 -0500

    Use word instead of initial for block argument

--- spec/support/shared_examples_for_sortings.rb
+++ spec/support/shared_examples_for_sortings.rb
@@ -1,7 +1,7 @@
 shared_examples 'returns the single record' do
   let(:only_record) { double('OnlyRecord') }
   let(:records) { [only_record] }
-  before { messages.each { |m| allow(only_record).to receive(m) } }
+  before { messages.each { |message| allow(only_record).to receive(message) } }
   it { is_expected.to eq([only_record]) }
 end
 
