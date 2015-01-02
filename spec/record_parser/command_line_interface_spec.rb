b0494ffdbcaba1c659f341c6d13d9eeed2fe3866
Amar Shah <lssdamperinspector@gmail.com>
2015-01-02 10:10:54 -0500

    Organize CommandLineInterface unit spec
    Clean up context setup and use shared example.

--- spec/record_parser/command_line_interface_spec.rb
+++ spec/record_parser/command_line_interface_spec.rb
@@ -2,20 +2,25 @@ require 'spec_helper'
 
 module RecordParser
   describe CommandLineInterface do
-    let(:application) { CommandLineInterface.new(out) }
-    let(:out) { double('Out') }
-    before { allow(out).to receive(:puts) }
     describe '#run' do
+      let(:run_application) { application.run(instruction, file) }
+      let(:application) { CommandLineInterface.new(out) }
+      let(:out) { double('Out') }
+      let(:file) { double('File') }
+      before do
+        allow(out).to receive(:puts)
+        allow(file).to receive(:read).and_return('')
+      end
+
       context 'with recognized sorting instruction' do
-        shared_examples 'executes sorting' do |sorting_class, instruction|
-          context "with instruction: #{instruction}" do
-            let(:file) { double('File') }
+        shared_examples 'executes sorting' do |sorting_class, good_instruction|
+          context "with instruction: #{good_instruction}" do
+            let(:instruction) { good_instruction }
             let(:sorting) { double('Sorting') }
             let(:record_one) { double('RecordOne') }
             let(:record_two) { double('RecordTwo') }
             let(:unsorted) { [record_one, record_two] }
             let(:sorted) { unsorted.reverse }
-            let(:run_application) { application.run(instruction, file) }
             before do
               allow(file).to receive(:read).and_return("RecordOne\nRecordTwo\n")
               allow(RecordParser::Record).to receive(:new).
@@ -51,25 +56,25 @@ module RecordParser
                         Sorting::ByLastNameDescending,
                         'last-name-descending'
       end
+
       context 'with bad input' do
-        let(:file) { double('File') }
-        before { allow(file).to receive(:read) }
         shared_examples 'does not raise an error' do
           subject { -> { run_application } }
           it { is_expected.not_to raise_error }
         end
+        shared_examples 'notifies the user' do |message|
+          it "displays '#{message}'" do
+            expect(out).to receive(:puts).with(message)
+            run_application
+          end
+        end
         context 'with unrecognized sorting instruction' do
           shared_examples 'handles unrecognized instruction' do |bad_instruction|
-            let(:run_application) { application.run(bad_instruction, file) }
-            before { allow(file).to receive(:read).and_return('') }
+            let(:instruction) { bad_instruction }
             context "when '#{bad_instruction}'" do
               it_has_behavior 'does not raise an error'
-              it 'notifies the user' do
-                expect(out).to receive(:puts).with(
-                  "Unrecognized instruction: #{bad_instruction}"
-                )
-                run_application
-              end
+              it_has_behavior 'notifies the user',
+                              "Unrecognized instruction: #{bad_instruction}"
             end
           end
           it_has_behavior 'handles unrecognized instruction', 'middle-name'
@@ -77,19 +82,15 @@ module RecordParser
         end
         context 'with unreadable file' do
           shared_examples 'handles unreadable file' do |bad_filename|
-            let(:run_application) { application.run('birth-date', file) }
+            let(:instruction) { 'birth-date' }
             before do
               allow(file).to receive(:read).and_raise(Errno::ENOENT)
               allow(file).to receive(:filename).and_return(bad_filename)
             end
             context "when '#{bad_filename}" do
               it_has_behavior 'does not raise an error'
-              it 'notifies the user' do
-                expect(out).to receive(:puts).with(
-                  "Could not read file: #{bad_filename}"
-                )
-                run_application
-              end
+              it_has_behavior 'notifies the user',
+                              "Could not read file: #{bad_filename}"
             end
           end
           it_has_behavior 'handles unreadable file', 'my_fiels.txt'
