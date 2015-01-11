1ea57a6534428b3977b9183cf98b34c2a27199e0
Amar Shah <amar47shah@gmail.com>
2015-01-11 12:28:31 -0500

    Nest specs in module declaration
    Not necessary in this case because the API module is loaded in
    api_spec_helper, but changed for consistent style.

--- spec/api/get_by_birth_date_spec.rb
+++ spec/api/get_by_birth_date_spec.rb
@@ -1,8 +1,10 @@
 require 'api_spec_helper'
 require 'support/shared_examples/for_sorting_endpoints'
 
-describe API::GetByBirthDate do
-  it_behaves_like 'a sorting endpoint',
-                  'records/birth_date',
-                  RecordParser::Sorting::ByBirthDate
+module API
+  describe GetByBirthDate do
+    it_behaves_like 'a sorting endpoint',
+                    'records/birth_date',
+                    RecordParser::Sorting::ByBirthDate
+  end
 end
