e5dc8407884cab94a0ac8fadd38b2e57d1a10c72
Amar Shah <amar47shah@gmail.com>
2015-01-11 11:59:27 -0500

    Move sorting endpoint specs to shared example set
    Expecting to use these shared examples for other sorting endpoints.

--- spec/api/get_by_birth_date_spec.rb
+++ spec/api/get_by_birth_date_spec.rb
@@ -1,39 +1,9 @@
 require 'spec_helper'
-require 'rack/test'
 require 'api'
-require 'support/matchers/eq_json'
+require 'support/shared_examples/for_sorting_endpoints'
 
 describe API::GetByBirthDate do
-  include Rack::Test::Methods
-
-  def app
-    API::Base
-  end
-
-  describe 'GET /records/birth_date' do
-    let(:send_request) { get 'records/birth_date' }
-    let(:records) { double('Records') }
-    let(:sorting) { double('Sorting') }
-    let(:sorted) { double('Sorted') }
-    let(:sorted_json) { { sorted: 'json' }.to_json }
-    before do
-      allow(API::Store).to receive(:records).and_return(records)
-      allow(RecordParser::Sorting::ByBirthDate).to receive(:new).
-                                                   and_return(sorting)
-      allow(sorting).to receive(:sort).and_return(sorted)
-      allow(sorted).to receive(:to_json).and_return(sorted_json)
-    end
-    it 'instantiates a sorting by birth date' do
-      expect(RecordParser::Sorting::ByBirthDate).to receive(:new).with(records)
-      send_request
-    end
-    it 'sorts' do
-      expect(sorting).to receive(:sort)
-      send_request
-    end
-    it 'renders the sorted records as JSON' do
-      send_request
-      expect(last_response.body).to eq_json(sorted_json)
-    end
-  end
+  it_behaves_like 'a sorting endpoint',
+                  'records/birth_date',
+                  RecordParser::Sorting::ByBirthDate
 end
