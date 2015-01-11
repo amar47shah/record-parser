ab735c9aa187c34ad48a1519cf8a32afdd571d20
Amar Shah <amar47shah@gmail.com>
2015-01-11 13:00:19 -0500

    Handle replacement of API::Store file
    All feature tests pass as of this commit.
    
    This is needed to pass feature tests. When Cucumber attempted to
    change the API::Store file, the old records were memoized and not
    overwritten, causing the test to fail (falsely, from production
    standpoint).

--- lib/api/store.rb
+++ lib/api/store.rb
@@ -1,8 +1,13 @@
 module API
   class Store
     class << self
-      attr_writer :file, :records
+      attr_writer :records
       undef_method :new
+
+      def file=(file)
+        @file = file
+        @records = nil
+      end
 
       def records
         @records ||= RecordParser::Reader.new(@file).records
--- spec/api/store_spec.rb
+++ spec/api/store_spec.rb
@@ -2,9 +2,6 @@ require 'api_spec_helper'
 
 module API
   describe Store do
-    let(:file) { double('File') }
-    before { Store.file = file }
-
     describe '::new' do
       subject { -> { Store.new } }
       it { is_expected.to raise_error(NoMethodError) }
@@ -12,21 +9,31 @@ module API
 
     describe '::records' do
       subject { Store.records }
-      let(:records) { double('FileRecords') }
-      context 'on initial access' do
-        before do
-          Store.records = nil
-          reader = double('Reader')
-          allow(RecordParser::Reader).to receive(:new).
-                                         with(file).
-                                         and_return(reader)
-          allow(reader).to receive(:records).and_return(records)
-        end
-        it { is_expected.to eq(records) }
+      let(:file) { double('File') }
+      let(:records) { double('Records') }
+      let(:reader) { double('Reader') }
+      before do
+        Store.file = file
+        allow(RecordParser::Reader).to receive(:new).and_return(reader)
       end
-      context 'on subsequent access' do
-        before { Store.records = records }
-        it { is_expected.to eq(records) }
+      context 'with original file' do
+        context 'on initial access' do
+          before { allow(reader).to receive(:records).and_return(records) }
+          it { is_expected.to eq(records) }
+        end
+        context 'on subsequent access' do
+          before { Store.records = records }
+          it { is_expected.to eq(records) }
+        end
+      end
+      context 'when file replaced' do
+        let(:another_file) { double('AnotherFile') }
+        let(:other_records) { double('OtherRecords') }
+        before do
+          Store.file = another_file
+          allow(reader).to receive(:records).and_return(other_records)
+        end
+        it { is_expected.to eq(other_records) }
       end
     end
   end
